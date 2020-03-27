0a17f129a0adc0b48e0e5622a2d4e57e53209c5f
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 20:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Polygon {
 class Triangle extends Polygon {
   get isValid() {
     if(this.countSides == 3) {
-      if((this.sides[0]+this.sides[1] < )
+      if((this.sides[0]+this.sides[1] > this.sides[2]) && (this.sides[0]+this.sides[1] > this.sides[2])
     } else {
       return false
     }
